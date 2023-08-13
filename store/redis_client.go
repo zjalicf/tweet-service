2c885ff805fc15fd4ee2c2215112834dbaf8deae
Filip Zjalic <zjalic@gmail.com>
2023-08-13 10:51:18 +0200

    feat: 2

--- store/redis_client.go
+++ store/redis_client.go
@@ -18,7 +18,7 @@ func GetRedisClient(host, port string) (*redis.Client, error) {
 	if err != nil {
 		log.Printf("failed to ping cache db because of: %s ", err)
 		// log.Print("hi")
-		log.Printf("change")
+		log.Printf("change2")
 		return nil, err
 	}
 
